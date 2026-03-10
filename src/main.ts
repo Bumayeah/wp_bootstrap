23c280893b4e3972ccedab56eb028107499edf77
Johnny Ceelen <jceelen@eudonet.com>
2026-03-10 20:36:48 +0100

    Add bootstrap module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './scss/styles.scss'
 
-import * as bootstrap from 'bootstrap'
+import 'bootstrap'
 
 import App from './App.vue'
 import router from './router'
